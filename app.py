c94736e06fdbc98b11e7c88eed0dc2eb2c953a1c
Mohan <mohanji2526@gmail.com>
2026-06-13 10:20:57 +0530

    updated code

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template, request, send_from_directory
 from flask_socketio import SocketIO, emit, join_room, leave_room
 import os
